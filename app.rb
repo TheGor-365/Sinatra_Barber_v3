afc31c7a58465d3914eb5962aad94a43812d9470
Gor <7495500@gmail.com>
2020-04-13 19:51:39 +0300

    Add erb pages

--- app.rb
+++ app.rb
@@ -1,8 +1,22 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+end
+
+get '/about' do
+
+	erb :about
+end
+
+get '/visit' do
+
+	erb :visit
+end
+
+get '/contacts' do
+
+	erb :contacts
 end
